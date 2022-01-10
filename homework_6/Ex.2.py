b519b1a9f2b1ac202dec1e2de2275a5c3d84da44
gnomfonhare <gnomfonhare@bk.ru>
2022-01-10 17:29:49 +0300

    Un.6 Ex.2 remade

--- homework_6/Ex.2.py
+++ homework_6/Ex.2.py
@@ -4,9 +4,9 @@ class Road:
         self._lenght = lenght
         self._width = width
 
-    def mass_calculation(self):
-        return(self._lenght * self._width * 25 * 5 / 1000)
+    def mass_calculation(self, a, b):
+        return(self._lenght * self._width * a * b / 1000)
 
 
 my_road = Road(5000, 20)
-print(f'{my_road.mass_calculation()}т')
+print(f'{my_road.mass_calculation(25, 5)}т')
