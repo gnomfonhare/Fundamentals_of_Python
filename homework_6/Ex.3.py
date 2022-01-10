45389b0a48a5d5997cfdea0e362e3274667a04f4
gnomfonhare <gnomfonhare@bk.ru>
2022-01-10 17:33:36 +0300

    Un.6 Ex.3 remade

--- homework_6/Ex.3.py
+++ homework_6/Ex.3.py
@@ -1,10 +1,10 @@
 class Worker:
 
-    def __init__(self, name, surname, position, income):
+    def __init__(self, name, surname, position, wage, bonus):
         self.name = name
         self.surname = surname
         self.position = position
-        self._income = income
+        self._income = {"wage": wage, "bonus": bonus}
 
 
 class Position(Worker):
@@ -16,7 +16,7 @@ class Position(Worker):
         return(sum(self._income.values()))
 
 
-worker_1 = Position('Иван', 'Иванов', 'Повар', {"wage": 50000, "bonus": 5000})
-worker_2 = Position('Пётр', 'Петров', 'Уборщик', {"wage": 30000, "bonus": 3000})
+worker_1 = Position('Иван', 'Иванов', 'Повар', 50000, 5000)
+worker_2 = Position('Пётр', 'Петров', 'Уборщик', 30000, 3000)
 print(f'{worker_1.get_full_name()} {worker_1.get_total_income()}')
 print(f'{worker_2.get_full_name()} {worker_2.get_total_income()}')
